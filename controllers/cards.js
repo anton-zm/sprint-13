5a021dde9fde1338e3628daef8484f2d538a634a
Anton Zmanovskii <antonzm34@gmail.com>
2020-07-03 16:13:13 +0400

    refactorinc orFail

--- controllers/cards.js
+++ controllers/cards.js
@@ -14,11 +14,11 @@ module.exports.createCard = (req, res) => {
     .catch(() => res.status(500).send({ message: 'Что-то пошло не так' }));
 };
 
-module.exports.deleteCard = async function findByIdAndRemove(req, res) {
-  const Card = await card.findByIdAndRemove(req.params.cardId);
-  if (Card == null) {
+module.exports.deleteCard = async (req, res) => {
+  const cardObj = await card.findByIdAndRemove(req.params.cardId);
+  if (cardObj == null) {
     res.status(404).send({ message: 'Карточка не найдена' });
   } else {
-    res.send({ Card });
+    res.send({ cardObj });
   }
 };
